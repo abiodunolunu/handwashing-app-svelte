f2d4e1c530a85f56d79b90c2e1c65470ea095b76
abeydev <abiodunolunu@gmail.com>
2020-12-12 21:36:12 +0100

    commiting html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,18 @@
- <h1>Test</h1>
+<script>
+import HowTo from "./HowTo.svelte";
+import Timer from "./Timer.svelte";
+</script>
+
+<style>
+    h1, h3 {
+        text-align: center;
+    }
+</style>
+
+<h1>Handwashing App</h1>
+
+<Timer />
+
+<HowTo />
+
+<h3><a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a> <a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a> </h3>
---
+++ src/ProgressBar.svelte
@@ -0,0 +1,19 @@
+<style>
+    .progress-container {
+        margin: 20px 0;
+        border: rgb(194, 194, 194) 1px solid;
+    }
+
+    .progress-bar {
+        height: 30px;
+        background-color: rgb(77, 199, 55);
+    }
+</style>
+
+<div bp="grid">
+    <div  bp="offset-5@md 4@md 12@sm" class="progress-container">
+        <div class="progress-bar" style="width: 20%">
+            <span class="sr-only">%</span>
+        </div>
+    </div>
+</div>
